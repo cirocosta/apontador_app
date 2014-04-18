fdab46af342293532b32d39f2dbf62d5647deb2b
Ciro S. Costa <ciro.costa@usp.br>
2014-04-18 14:17:33 -0300

    updates CLI, removes js and css hardcoded file names and  refactoring

--- examples/app.js
+++ examples/app.js
@@ -1,1 +1,3 @@
-console.log("dahora a hsiudashiuasdhiudsah");
+console.log("dahora a  asd sd hsiudashiuasdhiudsahdssaddsdasjiuss");
+alert('wow');
+console.log("dasiuhadsuih");
--- src/app.js
+++ src/app.js
@@ -14,46 +14,56 @@ var express = require('express');
 var request = require('request');
 var myUtils = require('./util');
 
+var cliAnswers;
 var app = express(),
-    server,
-    webpageUrl = '',
-    io = null,
-    staticDir = '',
-    watchDir = '',
-    useSocket = false,
-    args = process.argv.slice(2);
+  server,
+  io = null,
+  answers = {},
+  args = process.argv.slice(2);
 
 /**
  * Configures server settings based on cli args.
  */
 if (args.length) {
-    args.forEach(function (arg) {
-        var thisArg = arg.split('=');
-        var flag = thisArg[0];
+  args.forEach(function (arg) {
+    var thisArg = arg.split('=');
+    var flag = thisArg[0];
 
-        switch (flag) {
-        case '--webpage':
-            webpageUrl = thisArg[1];
-            break;
+    answers.files = {};
 
-        case '--dir':
-            staticDir = path.resolve(thisArg[1]);
-            break;
+    switch (flag) {
+    case '--webpage':
+      var webpage = thisArg[1];
+      answers.webpage = webpage;
 
-        case '--watch':
-            useSocket = true;
-            break;
+      if (webpage.protocol !== 'http:' || webpage.protocol !== 'https:') {
+        answers.webpage = 'http://' + answers.webpage;
+      }
+      break;
 
-        default:
-            console.log("Invalid arg.");
-            process.exit(1);
-        }
-    });
+    case '--dir':
+      answers.dir = path.resolve(thisArg[1]);
+      break;
+
+    case '--watch':
+      answers.watch = true;
+      break;
+
+    case '--jsfile':
+      answers.files.js = thisArg[1];
+      break;
+
+    case '--cssfile':
+      answers.files.css = thisArg[1];
+      break;
+
+    default:
+      console.log("Invalid arg.");
+      process.exit(1);
+    }
+  });
 } else {
-    console.error("Error: no args specified.");
-    console.error("Specify a webpage (--webpage=WEBPAGE) and a static dir " +
-                "(--dir=DIRECTORY) to proceed.");
-    process.exit(1);
+  answers = require('./cli-questions');
 }
 
 /**
@@ -70,44 +80,49 @@ app.use(express.compress());
  */
 app.use(function (req, res, next) {
 
-    var requestUrl = req.url;
-    var pl = url.parse(requestUrl).path.split('/');
-    var fileServed = pl[pl.length - 1];
+  var requestUrl = req.url;
+  var pl = url.parse(requestUrl).path.split('/');
+  var fileServed = pl[pl.length - 1];
 
+  if (fileServed.match(/\.js/) || fileServed.match(/\.css/)) {
+    next();
+    return;
+  }
 
-    if (fileServed.match(/\.js/) || fileServed.match(/\.css/)) {
-        next();
-        return;
+  request(url.resolve(answers.webpage, requestUrl), function (err, resp, body) {
+
+    if (err || resp.statusCode !== 200) {
+      next(err);
+    } else {
+
+      var jsDir = answers.files.js
+          ? path.join(answers.dir, answers.files.js)
+          : null;
+      var cssDir = answers.files.css
+          ? path.join(answers.dir, answers.files.css)
+          : null;
+
+      if (answers.watch) {
+        res.end(body +
+            myUtils.genLoadScript(jsDir, cssDir,
+                                  'http://localhost:' + app.get('port')));
+      } else {
+        res.end(body +
+            myUtils.genLoadScript(answers.files.js,
+                                  answers.files.css));
+      }
     }
-
-    request(url.resolve(webpageUrl, requestUrl), function (err, resp, body) {
-
-        if (err || resp.statusCode !== 200) {
-            next(err);
-        } else {
-            if (useSocket) {
-                res.end(body +
-                        myUtils.genLoadScript(path.join(staticDir, 'app.js'),
-                                              path.join(staticDir, 'style.css'),
-                                              'http://localhost:' +
-                                               app.get('port')));
-            } else {
-                res.end(body +
-                        myUtils.genLoadScript(path.join(staticDir, 'app.js'),
-                                              path.join(staticDir, 'style.css')));
-            }
-        }
-    });
+  });
 });
 
-if (staticDir) {
-    app.use(staticDir, express.static(staticDir));
+if (answers.dir) {
+  app.use(answers.dir, express.static(answers.dir));
 }
 
 // Server Initialization
 
 server = http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 
@@ -115,22 +130,22 @@ server = http.createServer(app).listen(app.get('port'), function () {
  * Socket.io stuff
  */
 
-if (useSocket) {
-    io = require('socket.io').listen(server);
-    io.sockets.on('connection', socketsConnectionHandler);
+if (answers.watch) {
+  io = require('socket.io').listen(server);
+  io.sockets.on('connection', socketsConnectionHandler);
 }
 
 /**
  * Handles the connection of a socket into the sockets.io server.
  */
 function socketsConnectionHandler (socket) {
-    if (useSocket) {
-        fs.watch(staticDir, function (ev, filename) {
-            if (ev === 'rename') {
-                // do something if it was just a rename
-            } else if (ev === 'change') {
-                socket.emit('reload', {data: 'reload the browser'});
-            }
-        });
-    }
+  if (answers.watch) {
+    fs.watch(answers.dir, function (ev, filename) {
+      if (ev === 'rename') {
+        // do something if it was just a rename
+      } else if (ev === 'change') {
+        socket.emit('reload', {data: 'reload the browser'});
+      }
+    });
+  }
 };
